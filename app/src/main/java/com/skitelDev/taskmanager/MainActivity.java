63ce3713d65edba36e987500218ce666f96d8cd8
BBeHumble <ilgiz.gabdrafikov@gmail.com>
2020-01-29 00:46:28 +0300

    Sqlite connection and get all request

--- app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
+++ app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
@@ -2,7 +2,11 @@
 package com.skitelDev.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        createDatabase();
+
+    }
+    public void createDatabase() {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS tasklist (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)");
+//        db.execSQL("INSERT INTO tasklist(text) VALUES ('поесть');");
+        Cursor query = db.rawQuery("SELECT * FROM tasklist;", null);
+        if(query.moveToFirst()){
+            do{
+                int id = query.getInt(0);
+                String text = query.getString(1);
+                Log.println(Log.WARN,"MESSAGE",id+" "+ text);
+            }
+            while(query.moveToNext());
+        }
+        query.close();
+        db.close();
     }
 }
