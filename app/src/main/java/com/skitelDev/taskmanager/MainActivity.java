d6008662cb14aa12c79a55326f74bf6f409a6555
BBeHumble <ilgiz.gabdrafikov@gmail.com>
2020-01-29 17:43:47 +0300

    add and getById queries

--- app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
+++ app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
@@ -9,29 +9,63 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.skitelDev.taskmanager.entities.Task;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createDatabase();
-
+        ArrayList<Task> list = new ArrayList();
+        insertIntoTaskList("новый таск", 1);
+        list = getTaskFromList(2);
+        for (Task task : list) {
+            Log.println(Log.ERROR, "MESSAGE", task.getId() + " " + task.getText());
+        }
     }
+
     public void createDatabase() {
         SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS tasklist (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)");
-//        db.execSQL("INSERT INTO tasklist(text) VALUES ('поесть');");
-        Cursor query = db.rawQuery("SELECT * FROM tasklist;", null);
-        if(query.moveToFirst()){
-            do{
-                int id = query.getInt(0);
-                String text = query.getString(1);
-                Log.println(Log.WARN,"MESSAGE",id+" "+ text);
-            }
-            while(query.moveToNext());
-        }
-        query.close();
+        db.execSQL("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)"); // для каждой конкретной задачи
+        db.execSQL("CREATE TABLE IF NOT EXISTS tasklist (id INTEGER, taskid INTEGER)");// таблица со списками таск листов(id - id листа - taskid - id таска, который содержится в листе
         db.close();
     }
+
+    //Получение списка задач по id списка
+    public ArrayList<Task> getTaskFromList(int taskListId) {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        ArrayList<Task> tasks = new ArrayList<>();
+        Cursor query = db.rawQuery("SELECT * FROM task " +
+                "JOIN tasklist " +
+                "ON tasklist.taskid == task.id " +
+                "WHERE tasklist.id == " + taskListId, null);
+        if (query.moveToFirst()) {
+            do {
+                int id = query.getInt(0);
+                String text = query.getString(1);
+                tasks.add(new Task(id, text));
+            }
+            while (query.moveToNext());
+        }
+        query.close();
+        return tasks;
+    }
+
+    public void insertIntoTaskList(String taskText, int tasklistid) {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("INSERT INTO task(text) VALUES ('" + taskText + "');");
+        Long index = findLastTaskID();
+        db.execSQL("INSERT INTO tasklist(id, taskid) VALUES (" + tasklistid + "," + index + ");");
+    }
+
+    public long findLastTaskID() {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        Cursor cursor = db.rawQuery("SELECT * FROM task;", null);
+        cursor.moveToLast();
+        return cursor.getLong(0);
+    }
+
 }
--- app/src/main/java/com/skitelDev/taskmanager/entities/Task.java
+++ app/src/main/java/com/skitelDev/taskmanager/entities/Task.java
@@ -1,4 +1,27 @@
 package com.skitelDev.taskmanager.entities;
 
 public class Task {
+    int id;
+    String text;
+
+    public Task(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
