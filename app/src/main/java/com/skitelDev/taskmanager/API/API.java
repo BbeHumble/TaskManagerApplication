aeaeaa145e72bf4b77d50423caabd87b2de2bf2e
BBeHumble <ilgiz.gabdrafikov@gmail.com>
2020-01-31 15:58:01 +0300

    refactioring

---
+++ app/src/main/java/com/skitelDev/taskmanager/API/API.java
@@ -0,0 +1,89 @@
+package com.skitelDev.taskmanager.API;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.skitelDev.taskmanager.entities.Task;
+
+import java.util.ArrayList;
+
+public class API {
+    public static void createDatabase(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)"); // для каждой конкретной задачи
+        db.execSQL("CREATE TABLE IF NOT EXISTS tasklist (id INTEGER, taskid INTEGER)");// таблица со списками таск листов(id - id листа - taskid - id таска, который содержится в листе
+        db.close();
+    }
+
+    //Получение списка задач по id списка
+    public static ArrayList<Task> getTaskFromList(SQLiteDatabase db, int taskListId) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Cursor query = db.rawQuery("SELECT * FROM task " +
+                "JOIN tasklist " +
+                "ON tasklist.taskid == task.id " +
+                "WHERE tasklist.id == " + taskListId, null);
+        if (query.moveToFirst()) {
+            do {
+                int id = query.getInt(0);
+                String text = query.getString(1);
+                tasks.add(new Task(id, text));
+            }
+            while (query.moveToNext());
+        }
+        query.close();
+        db.close();
+        return tasks;
+    }
+
+    public static void insertIntoTaskList(SQLiteDatabase db, String taskText, int tasklistid) {
+        db.execSQL("INSERT INTO task(text) VALUES ('" + taskText + "');");
+        long index = findLastTaskID(db);
+        db.execSQL("INSERT INTO tasklist(id, taskid) VALUES (" + tasklistid + "," + index + ");");
+        db.close();
+    }
+
+    public static long findLastTaskID(SQLiteDatabase db) {
+        Cursor cursor = db.rawQuery("SELECT * FROM task;", null);
+        cursor.moveToLast();
+        db.close();
+        return cursor.getLong(0);
+    }
+
+    public static void deleteTaskById(SQLiteDatabase db, int id) {
+        db.execSQL("DELETE FROM task WHERE id =" + id + ";");
+    }
+
+    public static void deleteTaskByName(SQLiteDatabase db, String text) {
+        db.execSQL("DELETE FROM task WHERE text ='" + text + "';");
+    }
+
+    public static Task findTaskById(SQLiteDatabase db, int id) {
+        Cursor cursor = db.rawQuery("SELECT * FROM task " +
+                "WHERE id =" + id + ";", null);
+        cursor.moveToFirst();
+        int resid = cursor.getInt(0);
+        String restext = cursor.getString(1);
+        db.close();
+        return new Task(resid, restext);
+
+    }
+    public static Task findTaskByText(SQLiteDatabase db, String text){
+        Cursor cursor = db.rawQuery("SELECT * FROM task " +
+                "WHERE text ='" + text + "';", null);
+        cursor.moveToFirst();
+        int resid = cursor.getInt(0);
+        String restext = cursor.getString(1);
+        db.close();
+        return new Task(resid, restext);
+    }
+    public static void updateTaskbyId(SQLiteDatabase db, int id, String text){
+        db.execSQL("UPDATE task " +
+                "SET text = '"+ text +"' " +
+                "WHERE id = "+id+";");
+    }
+    public static void updateTaskbyText(SQLiteDatabase db, String prev, String next){
+        db.execSQL("UPDATE task " +
+                "SET text = '"+ next +"' " +
+                "WHERE text = '"+prev+"';");
+    }
+
+}
--- app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
+++ app/src/main/java/com/skitelDev/taskmanager/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.skitelDev.taskmanager.API.API;
 import com.skitelDev.taskmanager.entities.Task;
 
 import java.util.ArrayList;
@@ -26,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomSheet();
-        
-        createDatabase();
-        ArrayList<Task> list = getTaskFromList(1);
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        API.createDatabase(db);
+        ArrayList<Task> list = API.getTaskFromList(db, 1);
         TaskListLoader(list);
     }
 
@@ -52,94 +53,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    public void createDatabase() {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)"); // для каждой конкретной задачи
-        db.execSQL("CREATE TABLE IF NOT EXISTS tasklist (id INTEGER, taskid INTEGER)");// таблица со списками таск листов(id - id листа - taskid - id таска, который содержится в листе
-        db.close();
-    }
 
-    //Получение списка задач по id списка
-    public ArrayList<Task> getTaskFromList(int taskListId) {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        ArrayList<Task> tasks = new ArrayList<>();
-        Cursor query = db.rawQuery("SELECT * FROM task " +
-                "JOIN tasklist " +
-                "ON tasklist.taskid == task.id " +
-                "WHERE tasklist.id == " + taskListId, null);
-        if (query.moveToFirst()) {
-            do {
-                int id = query.getInt(0);
-                String text = query.getString(1);
-                tasks.add(new Task(id, text));
-            }
-            while (query.moveToNext());
-        }
-        query.close();
-        db.close();
-        return tasks;
-    }
-
-    public void insertIntoTaskList(String taskText, int tasklistid) {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("INSERT INTO task(text) VALUES ('" + taskText + "');");
-        long index = findLastTaskID();
-        db.execSQL("INSERT INTO tasklist(id, taskid) VALUES (" + tasklistid + "," + index + ");");
-        db.close();
-    }
-
-    public long findLastTaskID() {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        Cursor cursor = db.rawQuery("SELECT * FROM task;", null);
-        cursor.moveToLast();
-        db.close();
-        return cursor.getLong(0);
-    }
-
-    public void deleteTaskById(int id) {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("DELETE FROM task WHERE id =" + id + ";");
-    }
-
-    public void deleteTaskByName(String text) {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("DELETE FROM task WHERE text ='" + text + "';");
-    }
-
-    public Task findTaskById(int id) {
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        Cursor cursor = db.rawQuery("SELECT * FROM task " +
-                "WHERE id =" + id + ";", null);
-        cursor.moveToFirst();
-        int resid = cursor.getInt(0);
-        String restext = cursor.getString(1);
-        db.close();
-        return new Task(resid, restext);
-
-    }
-    public Task findTaskByText(String text){
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        Cursor cursor = db.rawQuery("SELECT * FROM task " +
-                "WHERE text ='" + text + "';", null);
-        cursor.moveToFirst();
-        int resid = cursor.getInt(0);
-        String restext = cursor.getString(1);
-        db.close();
-        return new Task(resid, restext);
-    }
-    public void updateTaskbyId(int id, String text){
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("UPDATE task " +
-                "SET text = '"+ text +"' " +
-                "WHERE id = "+id+";");
-    }
-    public void updateTaskbyText(String prev, String next){
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("UPDATE task " +
-                "SET text = '"+ next +"' " +
-                "WHERE text = '"+prev+"';");
-    }
-    
 
 
 }
